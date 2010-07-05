360e32d11a4f78314076bf86f9723b880c6e7f72
Michael E. Gruen <mg@greyworks.com>
2010-07-05 16:31:33 -0400

    Made Task List sort by completeness

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,9 @@ class TasksController < ApplicationController
     @location = Location.find(params[:location_id])
     @task = @location.tasks.create!(params[:task])
     
+    @task.completed = false
+    @task.save
+    
     redirect_to @location
   end
 
@@ -13,5 +16,37 @@ class TasksController < ApplicationController
     
     redirect_to @location
   end
+  
+  def show
+    @task = Task.find(params[:id])
+    @location = Location.find(@task.location_id)
+    
+    redirect_to @location
+  end
+  
+  def complete
+    @task = Task.find(params[:id])
+    @task.completed = true
+    @task.save
+    
+    @location = Location.find(@task.location_id)
+    
+    redirect_to @location
+  end
+  
+  def uncomplete
+    @task = Task.find(params[:id])
+    @task.completed = false
+    @task.save
+    
+    @location = Location.find(@task.location_id)
+    
+    redirect_to @location
+  end
+    
+  def completed?
+    self.completed
+  end
+
   
 end
